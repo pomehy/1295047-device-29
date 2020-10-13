16a8859054341fe4dced87fcd6f183f4268b8471
pomehy <hlopkov.gleb@mail.ru>
2020-10-13 21:04:18 +0300

    add const mapModal

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,8 @@
 const mapLink = document.querySelector('.contacts__map');
+const mapModal = document.querySelector('.map-modal');
+
 const writeUsLink = document.querySelector('.contacts__link');
 const writeUsModal = document.querySelector('.write-us');
+
 const closeModal = document.querySelector('.close-modal');
+
