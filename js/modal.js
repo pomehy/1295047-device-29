e88267c729b900aa99e9c80825fe7af4e6bcebba
pomehy <hlopkov.gleb@mail.ru>
2020-10-13 21:02:50 +0300

    create modal.js и объявлены переменные

---
+++ js/modal.js
@@ -0,0 +1,4 @@
+const mapLink = document.querySelector('.contacts__map');
+const writeUsLink = document.querySelector('.contacts__link');
+const writeUsModal = document.querySelector('.write-us');
+const closeModal = document.querySelector('.close-modal');
