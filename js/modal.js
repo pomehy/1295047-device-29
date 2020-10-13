862c3ab4ac3acafbf96705b2a6e328c2f2bdbcc1
pomehy <hlopkov.gleb@mail.ru>
2020-10-13 22:10:01 +0300

    открыть / закрыть для модальных окон

--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,40 @@
-const mapLink = document.querySelector('.contacts__map');
-const mapModal = document.querySelector('.map-modal');
-
 const writeUsLink = document.querySelector('.contacts__link');
 const writeUsModal = document.querySelector('.write-us');
+const writeUsClose = document.querySelector('.write-us--close');
 
-const closeModal = document.querySelector('.close-modal');
+const mapLink = document.querySelector('.contacts__map');
+const mapModal = document.querySelector('.map-modal');
+const mapModalClose = document.querySelector('.map-modal--close');
 
-// Для карты
+
+// для карты
 
 mapLink.addEventListener('click', function(evt) {
   evt.preventDefault();
   mapModal.classList.add('modal-show');
+  console.log('открыто модальной окно с картой');
 });
 
-closeModal.addEventListener('click', function(evt) {
+mapModalClose.addEventListener('click', function(evt) {
   evt.preventDefault();
   mapModal.classList.remove('modal-show');
+  console.log('закрыто окно с картой');
 });
+
+
+// для формы обратной связи
+
+writeUsLink.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  writeUsModal.classList.add('modal-show');
+  console.log('открыто модальной окно с картой');
+});
+
+writeUsClose.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  writeUsModal.classList.remove('modal-show');
+  console.log('закрыто окно с формой');
+});
+
+
+
