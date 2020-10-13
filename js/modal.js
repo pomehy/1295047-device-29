fafb25399b9e7109fb4c54b68ecb4b6da4a39d9d
pomehy <hlopkov.gleb@mail.ru>
2020-10-13 21:14:17 +0300

    добавлены скрипты появления и закрытия mapModal

--- js/modal.js
+++ js/modal.js
@@ -6,3 +6,14 @@ const writeUsModal = document.querySelector('.write-us');
 
 const closeModal = document.querySelector('.close-modal');
 
+// Для карты
+
+mapLink.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  mapModal.classList.add('modal-show');
+});
+
+closeModal.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  mapModal.classList.remove('modal-show');
+});
